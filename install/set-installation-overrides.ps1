7b79d58d01f1a3fa0f83bce2175dc4d13a54be54
J.F. Larente <jfl@sitecore.net>
2018-02-14 12:56:19 -0500

    Configured set-installation settings

--- install/set-installation-overrides.ps1
+++ install/set-installation-overrides.ps1
@@ -1,50 +1,35 @@
 Param(
-    [string] $configurationFile = "configuration-xp0.json"
+    [string] $configurationFile = "configuration-xc0.json"
 )
 
 Write-Host "Setting Local Overrides in $configurationFile"
 
 $json = Get-Content -Raw $configurationFile |  ConvertFrom-Json
 
+# Assets and prerequisites
+
+$assets = $json.assets
+$assets.root = "$PSScriptRoot\assets"
+$assets.downloadFolder = Join-Path $assets.root "Downloads"
+#Commerce
+$assets.commerce.installationFolder = Join-Path $assets.root "Commerce"
 
 # Settings
-$assets = $json.assets
-$assets.licenseFilePath = Join-Path "c:\users\jfl\downloads" "license.xml"
-$json.assets = $assets
+
 # Site Settings
 $site = $json.settings.site
-$site.prefix = "habitat"
-$site.suffix = "dev.local"
-$site.webroot = "C:\inetpub\wwwroot"
-$site.hostName = $json.settings.site.prefix + "." + $json.settings.site.suffix
-$json.settings.site = $site
-
-$sql = $json.settings.sql
-# SQL Settings
-$sql.server = "."
-$sql.adminUser = "sa"
-$sql.adminPassword = "Sitecore12!@"
-$sql.minimumVersion="13.0.4001"
-
-$json.settings.sql = $sql
+$site.storefrontPrefix = "retail"
+$site.storefrontHostName = $site.storefrontPrefix + "." + $site.suffix
 
 # XConnect Parameters
 $xConnect = $json.settings.xConnect
-$xConnect.sqlCollectionUser = "collectionuser"
-$xConnect.sqlCollectionPassword = "Test12345"
 
-$json.settings.xConnect = $xConnect
+$xConnect.certificateConfigurationPath = Join-Path $assets.root "xconnect-createcert.json"
 
 # Sitecore Parameters
 $sitecore = $json.settings.sitecore
 
-$sitecore.siteName = [string]::Join(".", @($site.prefix, $site.suffix))
-$sitecore.siteRoot = Join-Path $site.webRoot -ChildPath $sitecore.siteName
-$json.settings.sitecore = $sitecore
 # Solr Parameters
 $solr = $json.settings.solr
-$solr.url = "https://localhost:8983/solr"
-$solr.root = "C:\solr\solr-6.6.2"
-$solr.serviceName = "solr-6.6.2"
 
-Set-Content $configurationFile  (ConvertTo-Json -InputObject $json -Depth 3)
+Set-Content $ConfigurationFile  (ConvertTo-Json -InputObject $json -Depth 4 )
