fc83d2098ed675c9823424fce0a44865129654c5
Svetlana <spareva@abv.bg>
2018-02-26 16:27:37 +0200

    commerce project

---
+++ src/Feature/Catalog/code/Controllers/CatalogController.cs
@@ -0,0 +1,28 @@
+﻿using Sitecore.Commerce.XA.Feature.Catalog.Repositories;
+using Sitecore.Commerce.XA.Foundation.Common;
+using Sitecore.Commerce.XA.Foundation.Common.Models;
+
+namespace Sitecore.Feature.Catalog.Controllers
+{
+    public class CatalogWithCartController : Commerce.XA.Feature.Catalog.Controllers.CatalogController
+    {
+        public CatalogWithCartController(IModelProvider modelProvider, IProductListHeaderRepository productListHeaderRepository,
+            IProductListRepository productListRepository, IPromotedProductsRepository promotedProductsRepository,
+            IProductInformationRepository productInformationRepository,
+            IProductImagesRepository productImagesRepository, IProductInventoryRepository productInventoryRepository,
+            IProductPriceRepository productPriceRepository, IProductVariantsRepository productVariantsRepository,
+            IProductListPagerRepository productListPagerRepository, IProductFacetsRepository productFacetsRepository,
+            IProductListSortingRepository productListSortingRepository,
+            IProductListPageInfoRepository productListPageInfoRepository,
+            IProductListItemsPerPageRepository productListItemsPerPageRepository,
+            ICatalogItemContainerRepository catalogItemContainerRepository, IStorefrontContext storefrontContext,
+            ISiteContext siteContext)
+            : base(modelProvider, productListHeaderRepository, productListRepository, promotedProductsRepository,
+                productInformationRepository, productImagesRepository, productInventoryRepository,
+                productPriceRepository, productVariantsRepository, productListPagerRepository, productFacetsRepository,
+                productListSortingRepository, productListPageInfoRepository, productListItemsPerPageRepository,
+                catalogItemContainerRepository, storefrontContext, siteContext)
+        {
+        }
+    }
+}
