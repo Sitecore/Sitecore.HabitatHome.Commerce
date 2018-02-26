b13e18d0bf32e7dad5ae34bedef839e4c6b0b9d7
John Lynn Beck <jbeck@hhogdev.com>
2018-02-26 14:50:38 -0500

    minicart styles and scripting

--- FrontEnd/-/media/Themes/Habitat Sites/Habitat Home/Habitat Home Store/scripts/components/minicart.js
+++ FrontEnd/-/media/Themes/Habitat Sites/Habitat Home/Habitat Home Store/scripts/components/minicart.js
@@ -1,45 +1,17 @@
 $(document).ready(function () {
-    var basket;
-    var miniCart;
+    //var basket;
+    //var miniCart;
 
-    (function init() {
-        selectClasses();
-    })();
+    var $miniCartComponent = $('.cxa-minicart-component'),
+        $button = $miniCartComponent.find('.top-text');    
 
-    function selectClasses() {
-        basket = $('.cxa-minicart-component').find('.basket');
-        miniCart = $(basket).find('.minicart');
-    }
-
-    $(basket).mousemove(function () {
-        if (!isMobile()) {
-            show();
+    $button.click(function () {        
+        if($miniCartComponent.hasClass('open')){
+            $miniCartComponent.parent().find('.open').removeClass('open');
+        }else {
+            $miniCartComponent.parent().find('.open').removeClass('open');
+            $miniCartComponent.addClass('open');
         }
     });
 
-    $(basket).mouseleave(function () {
-        hide();
-    });
-
-    $(basket).click(function () {
-        if (isMobile()) {
-            if ($(miniCart).css('display') === 'none') {
-                show();
-            } else {
-                hide();
-            }
-        }
-    });
-
-    function show() {
-        selectClasses();
-        $(miniCart).fadeTo('fast', 1);
-        $(miniCart).show();
-    }
-
-    function hide() {
-        selectClasses();
-        $(miniCart).fadeTo('fast', 0);
-        $(miniCart).hide();
-    }
 });
