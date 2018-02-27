78900336bbeac0ce6ccf6d729256464dd909cb2a
Svetlana <spareva@abv.bg>
2018-02-27 18:25:24 +0200

    add to cart from product list now almost working
    Related Work Items: #788

--- src/Feature/Catalog/code/Controllers/CatalogWithCartController.cs
+++ src/Feature/Catalog/code/Controllers/CatalogWithCartController.cs
@@ -5,14 +5,29 @@ using Sitecore.Commerce.XA.Foundation.Common.Models;
 using Sitecore.Commerce.XA.Foundation.Common.Models.JsonResults;
 using Sitecore.Commerce.XA.Foundation.Connect;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
 using System.Web.Mvc;
 using System.Web.UI;
+using Sitecore.Commerce.Engine.Connect;
+using Sitecore.Commerce.Engine.Connect.Interfaces;
+using Sitecore.Commerce.Engine.Connect.Search;
+using Sitecore.Commerce.XA.Foundation.Connect.Entities;
+using Sitecore.Commerce.XA.Foundation.Connect.Managers;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.Linq;
+using Sitecore.ContentSearch.SearchTypes;
+using Sitecore.ContentSearch.Security;
+using Sitecore.Data.Items;
 
 namespace Sitecore.Feature.Catalog.Controllers
 {
     public class CatalogWithCartController : Commerce.XA.Feature.Catalog.Controllers.CatalogController
     {
         public IAddToCartRepository AddToCartRepository { get; protected set; }
+        
 
         public IVisitorContext VisitorContext { get; protected set; }
 
@@ -47,8 +62,38 @@ namespace Sitecore.Feature.Catalog.Controllers
         [AllowAnonymous]
         [HttpPost]
         [OutputCache(Location = OutputCacheLocation.None, NoStore = true)]
-        public JsonResult AddCartLine(string addToCart_CatalogName, string addToCart_ProductId, string addToCart_VariantId, Decimal quantity)
+        public JsonResult AddCartLine(string addToCart_CatalogName, string addToCart_ProductId, string addToCart_VariantId, Decimal quantity = 1)
         {
+            //Sitecore.Commerce.XA.Foundation.Connect.Managers.SearchManager sm = new SearchManager(StorefrontContext);
+           
+            //var productItem = sm.GetProduct(addToCart_ProductId, addToCart_CatalogName);
+
+
+            using (IProviderSearchContext searchContext = CommerceTypeLoader.CreateInstance<ICommerceSearchManager>()
+                .GetIndex(addToCart_CatalogName).CreateSearchContext(SearchSecurityOptions.Default))
+            {
+                var query = searchContext.GetQueryable<CommerceSellableItemSearchResultItem>().Where(item =>
+                    item.CommerceSearchItemType == "SellableItem" && item.Language == Context.Language.Name &&
+                    item.Name == addToCart_ProductId.ToLowerInvariant());
+
+                var result = query.GetResults<CommerceSellableItemSearchResultItem>().Hits.FirstOrDefault()?.Document;
+
+                var productItem = Sitecore.Context.Database.GetItem(result?.SitecoreId);
+
+
+                if (productItem != null && productItem.HasChildren)
+                {
+                    //foreach (Item child in productItem.Children)
+                    //{
+                    VariantEntity model = this.ModelProvider.GetModel<VariantEntity>();
+                    model.Initialize(productItem.Children.FirstOrDefault());
+                    //variantEntityList.Add(model);
+                    addToCart_VariantId = model.VariantId;
+                    //}
+                }
+            }
+
+
             BaseJsonResult baseJsonResult;
             try
             {
@@ -62,9 +107,9 @@ namespace Sitecore.Feature.Catalog.Controllers
             return this.Json((object)baseJsonResult);
         }
 
-        protected override string GetIndexViewName()
+        public ActionResult ProductListWithCart()
         {
-            return "~/Views/Catalog/ProductListWithCart.cshtml";
+            return (ActionResult)this.View("~/Views/Catalog/ProductListWithCart.cshtml", (object)this.ProductListRepository.GetProductListRenderingModel());
         }
     }
 }
