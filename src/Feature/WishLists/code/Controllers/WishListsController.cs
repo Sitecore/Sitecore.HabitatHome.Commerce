12d9475d781a9396e5f5773f244943ef74fa0419
G.C. Blocker <37349565+GCXC@users.noreply.github.com>
2018-04-02 21:08:01 -0500

    Comment removal

--- src/Feature/WishLists/code/Controllers/WishListsController.cs
+++ src/Feature/WishLists/code/Controllers/WishListsController.cs
@@ -9,25 +9,6 @@ using System.Web.UI;
 namespace Sitecore.Feature.WishLists.Controllers
 {
     public class WishListsController : BaseCommerceStandardController
-    {
-      //  public OrderLinesController(IStorefrontContext storefrontContext, IVisitorContext visitorContext, IOrderLinesRepository orderLinesRepository)
-      //: base(storefrontContext)
-      //  {
-      //      Assert.IsNotNull((object)storefrontContext, nameof(storefrontContext));
-      //      Assert.IsNotNull((object)visitorContext, nameof(visitorContext));
-      //      Assert.IsNotNull((object)orderLinesRepository, nameof(orderLinesRepository));
-      //      this.OrderLinesRepository = orderLinesRepository;
-      //      this.VisitorContext = visitorContext;
-      //  }
-
-      //  protected IVisitorContext VisitorContext { get; set; }
-
-      //  protected IOrderLinesRepository OrderLinesRepository { get; set; }
-
-        //[HttpGet]
-        //public ActionResult WishListLines()
-        //{
-        //    return (ActionResult)this.View("~/Views/WishList/WishListLines.cshtml", (object)this.WishListLinesRepository.GetWishListLinesModel()); ;
-        //}
+    {      
     }
 }
