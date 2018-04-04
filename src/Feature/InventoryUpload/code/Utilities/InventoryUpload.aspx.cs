3b708fc20b1dfab2377b4247792842c13259cfda
J.F. Larente <jfl@sitecore.net>
2018-04-03 22:41:35 -0400

    Cleaning up code

--- src/Feature/InventoryUpload/code/Utilities/InventoryUpload.aspx.cs
+++ src/Feature/InventoryUpload/code/Utilities/InventoryUpload.aspx.cs
@@ -21,15 +21,12 @@ namespace Sitecore.Feature.InventoryUpload.HabitatUtility
         {
 
         }
-
-
-        // It is important to have currency populated at all places.
         protected void btnSubmit_Click(object sender, EventArgs e)
         {
             var inventory = txtInventoryJson.Text;
-            string startSpanRed = "<span style= \"color:red\">";
-            string startSpanGreen = "<span style= \"color:green\">";
-            string endSpan = "</span>";
+            const string startSpanRed = "<span style= \"color:red\">";
+            const string startSpanGreen = "<span style= \"color:green\">";
+            const string endSpan = "</span>";
 
             if (string.IsNullOrEmpty(inventory))
             {
@@ -41,41 +38,30 @@ namespace Sitecore.Feature.InventoryUpload.HabitatUtility
 
            var inventoryDetails= inputJson;
             {
-                
-
                 try
                 {
                     var ceConfig = (CommerceEngineConfiguration)Factory.CreateObject("commerceEngineConfiguration", true);
-                    var uri = new System.Uri(EngineConnectUtility.EngineConfiguration.ShopsServiceUrl);
 
                     Response.Write($" <br /> {startSpanGreen} Submitting inventory upload to authoring {endSpan}");
 
-                    var content = new System.Net.Http.StringContent(JsonConvert.SerializeObject(inventoryDetails));
+                    var content = new StringContent(JsonConvert.SerializeObject(inventoryDetails));
                     content.Headers.Remove("Content-Type");
                     content.Headers.Add("Content-Type", "application/json");
                    
                     var client = this.GetClient(ceConfig);
                     
                     var result = client.PostAsync("CreateStoreInventory", content).Result;
-                                        
-                    if (result.IsSuccessStatusCode)
-                    {
-                        Response.Write($" <br /> {startSpanGreen} SUCCESS submitting inventory to authoring {endSpan}");
-                        var response = result.Content.ReadAsStringAsync().Result;
-                        var responseModel = JsonConvert.DeserializeObject<InventoryResponse>(response);
 
-                    }
-                    else
-                    {
-                        Response.Write($" <br /> {startSpanRed}Failed to submit Inventory to authoring  {endSpan}");
-                    }
+                    Response.Write(result.IsSuccessStatusCode
+                        ? $" <br /> {startSpanGreen} SUCCESS submitting inventory to authoring {endSpan}"
+                        : $" <br /> {startSpanRed}Failed to submit Inventory to authoring  {endSpan}");
                 }
                 catch (Exception ex)
                 {
                     Response.Write($" <br /> {startSpanRed} ERROR: {ex.Message} {DateTime.Now} {endSpan}");
                 }
 
-                Tracker.Current.EndVisit(true);
+                
                 Response.Write($" <br /> {startSpanGreen} Processing Completed {endSpan}");
                 Response.Write($" <br /> {startSpanRed}-------------------------- {DateTime.Now} --------------------------------- {endSpan}");
             }
@@ -83,8 +69,6 @@ namespace Sitecore.Feature.InventoryUpload.HabitatUtility
             Response.Write($" <br /> {startSpanRed}Execution Completed at {DateTime.Now} {endSpan}");
 
         }
-
-   
         public HttpClient GetClient(CommerceEngineConfiguration config)
         {
             var httpClient = new HttpClient()
@@ -104,9 +88,7 @@ namespace Sitecore.Feature.InventoryUpload.HabitatUtility
             return httpClient;
 
         }
-    }
-
-    
+    }    
     public class ImportInventoryModel
     {        
         public List<string> ProductToAssociate { get; set; }
@@ -127,10 +109,4 @@ namespace Sitecore.Feature.InventoryUpload.HabitatUtility
         public string Lat { get; set; }
         public string Long { get; set; }
     }
-
-    public class InventoryResponse
-    {
-        public string value { get; set; }
-    }
-
 }
