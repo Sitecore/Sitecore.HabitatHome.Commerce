96a6a318cf72d1d12fd11d63795053e6c2b6d686
G.C. Blocker <37349565+GCXC@users.noreply.github.com>
2018-04-11 15:26:56 -0500

    Add Store Locator Manager

---
+++ src/Foundation/StoreLocator/code/Managers/StoreLocatorManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Foundation.StoreLocator.Managers
+{
+    public class StoreLocatorManager
+    {
+    }
+}
