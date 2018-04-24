fd64c45c5c8884034880f911142ae4284f435939
Elena Mosoff <emosoff@hhogdev.com>
2018-04-24 08:38:12 -0400

    added add-to-cart links on the order detail page for product reordering from history
    Related Work Items: #810

---
+++ src/Feature/OrderLines/code/Models/OrderLineVariantRenderingModel.cs
@@ -0,0 +1,24 @@
+﻿using Sitecore.Commerce.Entities;
+using Sitecore.Commerce.Entities.Carts;
+using Sitecore.Commerce.XA.Foundation.Common;
+using Sitecore.Commerce.XA.Foundation.Connect.Managers;
+using Sitecore.Commerce.XA.Feature.Account.Models;
+using Sitecore.Commerce.Engine.Connect.Entities;
+
+namespace Sitecore.Feature.OrderLines.Models
+{
+    public class OrderLineVariantRenderingModel  : OrderLineRenderingModel
+    {
+        public OrderLineVariantRenderingModel(IStorefrontContext storefrontContext, ISearchManager searchManager) : base(storefrontContext, searchManager)
+        {
+        }
+
+        public string ProductVariantId { get; set; }
+
+        public override void Initialize(CartLine orderLine, ShippingInfo shipping, Party party)
+        {
+            base.Initialize(orderLine, shipping, party);
+            ProductVariantId = ((CommerceCartProduct)orderLine.Product).ProductVariantId;
+        }
+    }
+}
