0770c21fa9b7d234c2d840e2ddf237d2ae7a514f
Elena Mosoff <emosoff@sitecore1.onmicrosoft.com>
2018-05-03 21:20:47 +0000

    Merged PR 637: Product Search
    Product Search:
    - added Foundation.Search project to override default SXA implementation and include product results in both type ahead suggestions and site search results;
    - added Product Brand and Product Tags search facets (computed fields)
    - updated rendering variant and styles for site search results
    
    CatalogLinkProvider:
    - added Foundation.Catalog to override the default Commerce XA CatalogLinkProvider to fix issue with GetItemUrl - product and category links can now correctly resolve
    
    Related work items: #1323, #1324, #1325, #1326

---
+++ src/Foundation/Catalog/code/Pipelines/CatalogLinkProvider.cs
@@ -0,0 +1,23 @@
+﻿using Sitecore.Commerce.Engine.Connect.DataProvider.Extensions;
+using Sitecore.Data.Items;
+using Sitecore.Links;
+
+namespace Sitecore.Foundation.Catalog.Pipelines
+{
+    public class CatalogLinkProvider : Commerce.XA.Foundation.Catalog.Pipelines.CatalogLinkProvider 
+    {
+        public override string GetItemUrl(Item item, UrlOptions options)
+        {
+            if (item.IsCatalogItem())
+            {
+                return GetDynamicUrl(item, new LinkUrlOptions
+                {
+                    Language = options.Language, Site = options.Site.Name
+
+                });
+            }
+
+            return base.GetItemUrl(item, options);
+        }
+    }
+}
---
+++ src/Foundation/Search/code/ComputedFields/ProductBrand.cs
@@ -0,0 +1,26 @@
+﻿using Sitecore.Commerce.Engine.Connect.DataProvider.Extensions;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.ComputedFields;
+using Sitecore.Data.Items;
+
+namespace Sitecore.Foundation.Search.ComputedFields
+{
+    public class ProductBrand   : AbstractComputedIndexField
+    {
+        public override object ComputeFieldValue(IIndexable indexable)
+        {
+            Item item = indexable as SitecoreIndexableItem;
+            if (item == null)
+            {
+                return string.Empty;
+            }
+
+            if (item.IsCatalogItem())
+            {
+                return item["Brand"];
+            }
+
+            return string.Empty;
+        }
+    }
+}
