6b5c00d5f2659a7e6be9adc60bd9c25280f69069
Gilbert Blocker <gilbert.blocker@sitecore.com>
2018-05-14 16:28:42 +0000

    Merged PR 681: Resolved bug 1564 - Update to disable Add To Wish List Button when processing, gulp theme style
    Update to disable Add To Wish List Button when processing, gulp theme style updates
    
    Related work items: #1564

--- src/Feature/AddToWishList/code/Scripts/Commerce/Feature/AddToWishList/habitat.commerce.feature.addtowishlist.js
+++ src/Feature/AddToWishList/code/Scripts/Commerce/Feature/AddToWishList/habitat.commerce.feature.addtowishlist.js
@@ -2,20 +2,17 @@
     var self = this;
     self.ErrorMessage = ko.observable("");
     self.ProductId = ko.observable("");
-    self.VariantId = ko.observable("");   
+    self.VariantId = ko.observable("");
 
-    self.InitializeViewModel = function (productID, variantID) {          
+    self.InitializeViewModel = function (productID, variantID) {
         self.ProductId = productID;
         self.VariantId = variantID;
-        
     }
-
-    self.AddItemToWishList = function (item, event) {        
+    self.AddItemToWishList = function (item, event) {
         var itemQuantity = $('.add-to-cart-qty-input').val();
-        var sender = event.currentTarget;
+
         $(event.currentTarget).attr('disabled', 'disabled');
         $(event.currentTarget).find('span').text('ADDING TO MY WISH LIST ...');
-
         AjaxService.Post("/api/cxa/wishlistlines/AddWishListLine", { productId: self.ProductId, variantId: self.VariantId, quantity: itemQuantity }, function (data, success, sender) {
             if (success && data.Success) {
                 $(event.currentTarget).removeAttr('disabled');
@@ -24,25 +21,25 @@
         });
     }
     self.UpdateVariant = function (variantId) {
-        self.VariantId = variantId;        
+        self.VariantId = variantId;
     }
 }
 
 function InitializeAddToWishListButoon() {
-    
+
     AddToWishListVMController = new AddToWishListViewModel();
     if ($("#AddToWishList").length > 0) {
 
-        ko.applyBindings(AddToWishListVMController, document.getElementById("AddToWishList"));        
+        ko.applyBindings(AddToWishListVMController, document.getElementById("AddToWishList"));
         var productID = $("#addtocart_productid").val();
         var variantID = ($("#addtocart_variantid").val() != "" ? $("#addtocart_variantid").val() : $(".valid-variant-combo").children(":first").attr("id"));
 
         AddToWishListVMController.InitializeViewModel(productID, variantID);
         $("#variantColor").on('change', function () {
-            AddToWishListVMController.UpdateVariant($("#addtocart_variantid").val());           
+            AddToWishListVMController.UpdateVariant($("#addtocart_variantid").val());
         });
-    }    
+    }
 }
-$(document).ready(function () {    
+$(document).ready(function () {
     InitializeAddToWishListButoon();
 });
