a69b8272aa29c9d80d7721ba032595d62ec7c985
Gilbert Blocker <gilbert.blocker@sitecore.com>
2018-05-25 17:34:32 +0000

    Merged PR 773: Developed Active Promotion Feature to display on PDP
    - Developed ability to display all active promotions on related PDP
    
    Related work items: #1608, #1609, #279

---
+++ src/Feature/ActivePromotion/website/Repositories/IActivePromotionRepository.cs
@@ -0,0 +1,14 @@
+﻿using Sitecore.Commerce.XA.Foundation.Connect;
+using Sitecore.HabitatHome.Feature.ActivePromotion.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.HabitatHome.Feature.ActivePromotion.Repositories
+{
+    public interface IActivePromotionRepository
+    {
+        ActivePromotionRenderingModel GetActivePromotionRenderingModel(IVisitorContext visitorContext);
+    }
+}
---
+++ src/Foundation/Promotions/website/Models/Promotion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.HabitatHome.Foundation.Promotions.Models
+{
+    public class Promotion
+    {
+        public string Id { get; set; }
+        public string DisplayCartText { get; set; }
+        public List<dynamic> Qualifications { get; set; }
+        public List<dynamic> Benefits { get; set; }
+        public DateTime ValidFrom { get; set; }
+        public DateTime ValidTo { get; set; }
+
+        public Promotion()
+        {
+            this.ValidFrom = new DateTime();
+            this.ValidTo = new DateTime();
+            this.Qualifications = new List<dynamic>();
+            this.Benefits = new List<dynamic>();
+        }
+    }
+}
