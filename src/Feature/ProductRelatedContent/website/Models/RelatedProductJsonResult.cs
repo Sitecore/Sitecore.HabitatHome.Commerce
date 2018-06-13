60185b3062b052231e2e53dc465f05b4fce3115f
Gilbert Blocker <gilbert.blocker@sitecore.com>
2018-06-13 14:17:40 +0000

    Merged PR 798: Developed Product Related Content Feature
    - Developed related content tabs for PDP to include related products, cross sell products, upsell products, and documentation
    
    Related work items: #1672, #1673, #1674, #1693

---
+++ src/Feature/ProductRelatedContent/website/Models/RelatedProductJsonResult.cs
@@ -0,0 +1,16 @@
+﻿using Sitecore.Commerce.XA.Feature.Catalog.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.HabitatHome.Feature.ProductRelatedContent.Models
+{
+    public class RelatedProductJsonResult
+    {
+        public string ProductName { get; set; }
+        public string ImageSrc { get; set; }
+        public string ProductUrl { get; set; }
+        public string ListPrice { get; set; }
+    }
+}
---
+++ src/Feature/ProductRelatedContent/website/Repositories/IProductRelatedContentRepository.cs
@@ -0,0 +1,21 @@
+﻿using Sitecore.Commerce.XA.Feature.Catalog.Models;
+using Sitecore.Commerce.XA.Foundation.Common;
+using Sitecore.Commerce.XA.Foundation.Common.Models;
+using Sitecore.Commerce.XA.Foundation.Connect;
+using Sitecore.HabitatHome.Feature.ProductRelatedContent.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.HabitatHome.Feature.ProductRelatedContent.Repositories
+{
+    public interface IProductRelatedContentRepository
+    {
+        CatalogItemRenderingModel GetProductRelatedContentRenderingModel(IVisitorContext visitorContext);
+        IEnumerable<RelatedProductJsonResult> GetRelatedProducts(IModelProvider modelProvider, IStorefrontContext storefrontContext, IVisitorContext visitorContext, string productId);
+        IEnumerable<RelatedProductJsonResult> GetCrossSellProducts(IModelProvider modelProvider, IStorefrontContext storefrontContext, IVisitorContext visitorContext, string productId);
+        IEnumerable<RelatedProductJsonResult> GetUpSellProducts(IModelProvider modelProvider, IStorefrontContext storefrontContext, IVisitorContext visitorContext, string productId);
+        IEnumerable<ProductDocumentJsonResult> GetProductDocuments(IModelProvider modelProvider, IStorefrontContext storefrontContext, IVisitorContext visitorContext, string productId);
+    }
+}
