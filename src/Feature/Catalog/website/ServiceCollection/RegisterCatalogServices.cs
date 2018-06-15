3a4fb6b8aa4960f33040e3d65f573c3181adcde4
Elena Mosoff <emosoff@sitecore1.onmicrosoft.com>
2018-06-15 20:07:20 +0000

    Merged PR 830: initial POC for breadcrumbs on the product detail page
    initial POC for breadcrumbs on the product detail page
    
    Related work items: #1726

---
+++ src/Feature/Catalog/website/ServiceCollection/RegisterCatalogServices.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;          
+using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+using Sitecore.HabitatHome.Feature.Catalog.Repositories;
+
+namespace Sitecore.HabitatHome.Feature.Catalog.ServiceCollection
+{
+    public class RegisterCatalogServices : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {                                                                           
+            Replace<Sitecore.XA.Feature.Navigation.Repositories.Breadcrumb.IBreadcrumbRepository, BreadcrumbRepository>(serviceCollection, ServiceLifetime.Transient);
+        }
+
+        public IServiceCollection Replace<TService, TImplementation>(
+            IServiceCollection services,
+            ServiceLifetime lifetime)
+            where TService : class
+            where TImplementation : class, TService
+        {
+
+            var descriptorToRemove = services.FirstOrDefault(d => d.ServiceType == typeof(TService));  
+            services.Remove(descriptorToRemove);
+
+            var descriptorToAdd = new ServiceDescriptor(typeof(TService), typeof(TImplementation), lifetime);      
+            services.Add(descriptorToAdd);
+
+            return services;
+        }
+    }
+}
